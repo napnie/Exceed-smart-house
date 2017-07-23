4eec869db99e2a0a0070e65e8113a3dd7828390b
Nitith Chayakul <c.nitith@gmail.com>
2017-07-23 11:42:18 +0700

    add onclick property in slider

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
-    
-} )
+    var link = "1212312/OACEED/"
+
+    function send(varible, value) {
+        $.ajax({
+                url: link + varible + "/set/" + value
+            }).done(function () {
+
+            }).fail(function () {
+                
+            })
+    }
+
+    function autoClick(slider) {
+        if (slider.checked) {
+            
+        } else {
+            
+        }
+    }
+
+    function airClick(slider) {
+        if(slider.checked) {
+            send(airStatus, 1)
+        } else {
+            send(airStatus, 0)
+        }
+    }
+
+    function lightClick(slider) {
+        if(slider.checked) {
+            send(lightStatus, 1)
+        } else {
+            send(lightStatus, 0)
+        }
+    }
+})
