087bf49d7ad4444a6606e2ee60e0b60993bb576a
Nitith Chayakul <c.nitith@gmail.com>
2017-07-23 15:40:04 +0700

    fix automation bug

--- index.js
+++ index.js
@@ -1,88 +1,98 @@
 $(document).ready(function () {
     var link = "http://158.108.165.223/data/OACEED/"
 
-    function send(variable, value) {
+    function send(value, variable = "") {
+        console.log(link + variable + (variable ? "/" : "") + "set/" + value)
         $.ajax({
-            url: link + variable + "/set/" + value
+            url: link + variable + (variable ? "/" : "") + "set/" + value
         }).done(function () {
 
         }).fail(function () {
-            console.error(variable+" error!")
+            console.error(variable + " error!")
         })
-        console.log(variable + " " + value)
     }
-
-    function receive(variable) {
+    function turnBoolean(x) {
+        if (x == 0) {
+            return false
+        } else {
+            return true
+        }
+    }
+    function receive(variable, tag) {
         $.ajax({
-            url: link + variable
+            url: link + variable,
+
         }).done(function (data) {
-            return data
-        }).fail(function (data) {
-            console.error(variable + " " + data + " error!")
-            return 0
-        })
-        console.log("receive "+variable)
+            console.log(tag + " " + data)
+            $(tag).prop('checked', turnBoolean(data))
+        });
+
     }
 
     $('#auto-switch').change(function () {
         if ($(this).prop("checked")) {
-            send("", 1)
+            send(1, "autoStatus")
 
-            $('#light-switch').attr('disabled','disabled')
-            $('#air-switch').attr('disabled','disabled')
-            $('#door-switch').attr('disabled','disabled')
+            $('#light-switch').attr('disabled', 'disabled')
+            $('#air-switch').attr('disabled', 'disabled')
+            $('#door-switch').attr('disabled', 'disabled')
 
             return
         }
-        send("", 0)
+        send(0, "autoStatus")
 
         $('#light-switch').removeAttr('disabled')
         $('#air-switch').removeAttr('disabled')
         $('#door-switch').removeAttr('disabled')
     })
 
-    function turnBoolean(binary) {
-        if (binary == 0) {
-            return false
-        }
-        return true
+    function recieveData(variable, tag) {
+        $.ajax({
+            url: link + variable,
+        }).done(function (data) {
+            console.log(tag + " " + data)
+            $(tag).text(data)
+        });
     }
 
     setInterval(function () {
         if ($('#auto-switch').prop('checked')) {
-            $('#light-switch').prop('checked',  turnBoolean(receive("lightStatus")) )
-            $('#air-switch').prop('checked',  turnBoolean(receive("airStatus")) )
-            $('#door-switch').prop('checked',  turnBoolean(receive("doorStatus")) )
+            receive("lightStatus", '#light-switch')
+            receive("airStatus", '#air-switch')
+            receive("doorStatus", '#door-switch')
         }
-        var temp = receive("tempStatus")
-        var person = receive("personCount")
-        $('#temperature').text(temp)
-        $('#human').text(person)
+        // var temp = receive("tempStatus")
+        // var person = receive("personCount")
+        // $('#temperature').text(temp)
+        // $('#human').text(person)
+        recieveData("tempStatus", "#temperature")
+        recieveData("personCount", "#human")
+        // $('#human').text(0)
+        console.log("OK");
     }, 5000)
 
     $('#air-switch').change(function () {
         if ($(this).prop("checked")) {
-            send("airStatus", 1)
-            $('#temperature').text(1)
+            send(1, "airStatus")
             return
         }
-        send("airStatus", 0)
+        send(0, "airStatus")
     })
 
     $('#light-switch').change(function () {
         if ($(this).prop("checked")) {
-            send("lightStatus", 1)
+            send(1, "lightStatus")
             return
         }
-        send("lightStatus", 0)
+        send(0, "lightStatus")
     })
 
     $('#door-switch').change(function () {
         if ($(this).prop("checked")) {
-            send("doorStatus", 1)
+            send(1, "doorStatus")
             return
         }
-        send("doorStatus", 0)
+        send(0, "doorStatus")
     })
 
 })
