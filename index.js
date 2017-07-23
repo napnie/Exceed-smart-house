fb125ace628a696560411fdd06cdf03b92cd8f18
Nitith Chayakul <c.nitith@gmail.com>
2017-07-23 13:22:49 +0700

    connect page with server

--- index.js
+++ index.js
@@ -1,37 +1,88 @@
 $(document).ready(function () {
-    var link = "1212312/OACEED/"
+    var link = "http://158.108.165.223/data/OACEED/"
 
-    function send(varible, value) {
+    function send(variable, value) {
         $.ajax({
-                url: link + varible + "/set/" + value
-            }).done(function () {
+            url: link + variable + "/set/" + value
+        }).done(function () {
 
-            }).fail(function () {
-                
-            })
+        }).fail(function () {
+            console.error(variable+" error!")
+        })
+        console.log(variable + " " + value)
     }
 
-    function autoClick(slider) {
-        if (slider.checked) {
-            
-        } else {
-            
+    function receive(variable) {
+        $.ajax({
+            url: link + variable
+        }).done(function (data) {
+            return data
+        }).fail(function (data) {
+            console.error(variable + " " + data + " error!")
+            return 0
+        })
+        console.log("receive "+variable)
+    }
+
+    $('#auto-switch').change(function () {
+        if ($(this).prop("checked")) {
+            send("", 1)
+
+            $('#light-switch').attr('disabled','disabled')
+            $('#air-switch').attr('disabled','disabled')
+            $('#door-switch').attr('disabled','disabled')
+
+            return
         }
+        send("", 0)
+
+        $('#light-switch').removeAttr('disabled')
+        $('#air-switch').removeAttr('disabled')
+        $('#door-switch').removeAttr('disabled')
+    })
+
+    function turnBoolean(binary) {
+        if (binary == 0) {
+            return false
+        }
+        return true
     }
 
-    function airClick(slider) {
-        if(slider.checked) {
-            send(airStatus, 1)
-        } else {
-            send(airStatus, 0)
+    setInterval(function () {
+        if ($('#auto-switch').prop('checked')) {
+            $('#light-switch').prop('checked',  turnBoolean(receive("lightStatus")) )
+            $('#air-switch').prop('checked',  turnBoolean(receive("airStatus")) )
+            $('#door-switch').prop('checked',  turnBoolean(receive("doorStatus")) )
         }
-    }
+        var temp = receive("tempStatus")
+        var person = receive("personCount")
+        $('#temperature').text(temp)
+        $('#human').text(person)
+    }, 5000)
 
-    function lightClick(slider) {
-        if(slider.checked) {
-            send(lightStatus, 1)
-        } else {
-            send(lightStatus, 0)
+    $('#air-switch').change(function () {
+        if ($(this).prop("checked")) {
+            send("airStatus", 1)
+            $('#temperature').text(1)
+            return
         }
-    }
+        send("airStatus", 0)
+    })
+
+    $('#light-switch').change(function () {
+        if ($(this).prop("checked")) {
+            send("lightStatus", 1)
+            return
+        }
+        send("lightStatus", 0)
+    })
+
+    $('#door-switch').change(function () {
+        if ($(this).prop("checked")) {
+            send("doorStatus", 1)
+            return
+        }
+        send("doorStatus", 0)
+    })
+
 })
